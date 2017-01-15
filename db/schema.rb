74e3f5b3ed7ed8e829833f95405fd417860b2ebb
陈为林 <guangdongjoe@gmail.cm>
2017-01-16 00:27:22 +0800

    用scaffold新增了留言板

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110025531) do
+ActiveRecord::Schema.define(version: 20170115162357) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170110025531) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "attachment"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
