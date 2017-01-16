0cbb1f61d9106637b92db04312daf82361f7eb89
陈为林 <guangdongjoe@gmail.cm>
2017-01-17 03:24:55 +0800

    新增昵称到user表

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   def require_is_admin
     if !current_user.admin?
@@ -6,4 +7,11 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+ protected
+ def configure_permitted_parameters
+   added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
+   devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+   devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+ end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :resumes
+  attr_accessor :login
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+  
   def admin?
     is_admin
   end
